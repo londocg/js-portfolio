c59a2e5a48df51b8b941de9f436497d5ccfd1845
Leonardo Cabezas Grajales <numeraljoker@gmail.com>
2022-10-27 16:17:10 -0500

    se imprementa copy-plugin para copiar imgs

--- webpack.config.js
+++ webpack.config.js
@@ -43,5 +43,13 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin ({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
     ]
 }
